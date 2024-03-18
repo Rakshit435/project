864ceff6a8b5fea8927d4f2b1aa1688f5ffa1770
rakshit1133be21 <32881574-rakshit1133be21@users.noreply.replit.com>
2024-03-18 07:28:17 +0000

    second

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+app.use(express.json());
 
+app.post('/bfhl',(req,res)=>{
+  const data = req.body.data;
+
+  const evenNumber = data.filter(item => item % 2 === 0)
+  const oddNumber = data.filter(item => item % 2 !== 0)
+
+  const alphabet = data.filter(item => item.match(/[a-zA-Z]/).test(item));
+  const alphabets = alphabet.map(item => item.toUpperCase());
+
+  app.listen(3000,()=>{
+    console.log('server working on host 3000')
+  })
+  
+})
